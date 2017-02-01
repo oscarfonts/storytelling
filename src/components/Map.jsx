e82c48464b5de262738068d15d5a463d683d49c6
Oscar Fonts <oscar.fonts@gmail.com>
2017-02-01 17:00:58 +0100

    Custom, fullscreen map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,24 +7,31 @@ export default class Map extends Component {
     super(props);
     this.state = {
       viewport: {
-        latitude: 41,
-        longitude: 2,
-        zoom: 8,
+        hash: true,
+        latitude: 41.3949,
+        longitude: 2.1756,
+        zoom: 13,
         startDragLngLat: null,
         isDragging: false,
+        width: '100%',
+        height: '100%',
+        bearing: -45,
+        pitch: 0.001,
+        mapStyle: 'http://demo.fonts.cat:8001/styles/bright-v9.json',
       },
     };
+    this.state.viewport = Object.assign(this.state.viewport, props);
     this.onChangeViewport = this.onChangeViewport.bind(this);
   }
 
-  onChangeViewport(viewport) {
+  onChangeViewport(newViewport) {
+    const viewport = Object.assign({}, this.state.viewport, newViewport);
     this.setState({ viewport });
   }
 
   render() {
-    const viewport = Object.assign({}, this.state.viewport, this.props);
     return (<MapGL
-      {...viewport}
+      {...this.state.viewport}
       onChangeViewport={this.onChangeViewport}
     />);
   }
--- src/index.jsx
+++ src/index.jsx
@@ -3,25 +3,22 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+// import RaisedButton from 'material-ui/RaisedButton';
 import Map from './components/Map';
+
+import './styles.css';
 
 injectTapEventPlugin();
 
-const rootElement = document.createElement('div');
-document.body.appendChild(rootElement);
-
 ReactDOM.render(
   <MuiThemeProvider>
-    <div>
-      <Map
-        width={400} height={400}
-        mapboxApiAccessToken="pk.eyJ1Ijoib3NjYXJmb250cyIsImEiOiJ0VFU5eW1VIn0.4MelWeJR1VBftrI84tKULw"
-      />
-      <RaisedButton label="Material button" />
+    <div id="container">
+      <Map />
     </div>
   </MuiThemeProvider>,
-  rootElement,
+  document.body,
 );
+
+// <RaisedButton label="Material button" />
 
 export default true;
