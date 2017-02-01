229974df8a6caa79f55d9dac4740f618f8c004c6
Oscar Fonts <oscar.fonts@gmail.com>
2017-02-01 17:00:29 +0100

    Build with mapbox-gl

--- webpack.config.js
+++ webpack.config.js
@@ -7,57 +7,68 @@ var entryPath = path.join(__dirname, 'src');
 var outputPath = path.join(__dirname, 'dist');
 
 var htmlPlugin = new HtmlWebpackPlugin({
-    title: 'storytelling',
-    files: {
-        "css": "https://fonts.googleapis.com/css?family=Roboto:300,400,500"
-    }
+  title: 'storytelling',
+  files: {
+    "css": [
+      "https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+    ]
+  }
 });
 
 module.exports = {
-    context: entryPath,
-    entry: './index.jsx',
-    output: {
-        path: outputPath,
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [".js", ".jsx"],
-        alias: {
-            webworkify: 'webworkify-webpack-dropin'
-        }
-    },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /(node_modules)/,
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: ['react', 'es2015'],
-                    plugins: ['react-html-attrs', 'transform-class-properties']
-                }
-            }, {
-                loader: 'eslint-loader'
-            }]
-        },{
-            include: /node_modules\/mapbox-gl.*\.js$/,
-            loader: 'transform-loader?brfs-babel',
-            enforce: 'post'
-        }]
-    },
-    plugins: debug ? [
-        htmlPlugin,
-    ] : [
-        htmlPlugin,
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-        })
-    ],
-    devtool: debug ? 'inline-source-map' : false,
-    devServer: {
-        contentBase: outputPath
-    },
-    node: {
-      fs: 'empty'
+  context: entryPath,
+  entry: './index.jsx',
+  output: {
+    path: outputPath,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      webworkify: 'webworkify-webpack-dropin'
     }
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules)/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['react', 'es2015'],
+          plugins: ['react-html-attrs', 'transform-class-properties']
+        }
+      }, {
+        loader: 'eslint-loader'
+      }]
+    }, {
+      test: /node_modules\/mapbox-gl.*\.js$/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }, {
+        loader: 'transform-loader?brfs'
+      }]
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  plugins: debug ? [
+      htmlPlugin,
+    ] : [
+      htmlPlugin,
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+      })
+    ],
+  devtool: debug ? 'inline-source-map' : false,
+  devServer: {
+    contentBase: outputPath
+  },
+  node: {
+    fs: 'empty'
+  }
 };
