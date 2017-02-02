b81fbd0bad9163c6f24988c00f3f4fa1452e6049
Oscar Fonts <oscar.fonts@gmail.com>
2017-02-02 10:02:16 +0100

    Simpler, stateless map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,28 +3,21 @@ import React, { Component } from 'react';
 import ReactMapboxGl from 'react-mapbox-gl';
 
 export default class Map extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      viewport: {
-        style: 'http://demo.fonts.cat:8001/styles/bright-v9.json',
-        accessToken: 'ReactMapboxGl makes this property mandatory but it is useless w/custom layer',
-        center: [2.1756, 41.3949],
-        zoom: [13],
-        bearing: -45,
-        containerStyle: {
-          height: '100%',
-        },
-        hash: true,
-      },
-    };
-    this.state.viewport = Object.assign(this.state.viewport, props);
-  }
+  static defaultProps = {
+    style: 'http://demo.fonts.cat:8001/styles/bright-v9.json',
+    accessToken: 'ReactMapboxGl makes this property mandatory but it is useless w/custom layer',
+    center: [2.1756, 41.3949],
+    zoom: [13],
+    bearing: -45,
+    containerStyle: {
+      height: '100%',
+    },
+    hash: true,
+  };
 
   render() {
-    const viewport = Object.assign(this.state.viewport, this.props);
     return (<ReactMapboxGl
-      {...viewport}
+      {...this.props}
     />);
   }
 }
